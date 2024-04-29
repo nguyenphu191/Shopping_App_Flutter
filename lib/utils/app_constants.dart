4c6374370e537d4531dea44e003b015e1c2a624b
nguyenphu191 <149788592+nguyenphu191@users.noreply.github.com>
2024-04-29 16:56:53 +0700

    Delete lib directory

--- lib/utils/app_constants.dart
+++
@@ -1,8 +0,0 @@
-class AppConstants {
-  static const String APP_NAME = "FuFu";
-  static const int APP_VERSION = 1;
-  static const String BASE_URL = "http://localhost:3000";
-  static const String POPULAR_PRODUCT_URI = "/api/v1/products/popular";
-  static const String REGISTER_URI = "/user";
-  static const String TOKEN = "DBtoken";
-}
